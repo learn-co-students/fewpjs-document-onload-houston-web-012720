ad3d14664561e574e5d238b600ebb8b0ea098187
somaia-khalil <sumaya_khair@live.com>
2020-03-09 22:33:15 -0500

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,6 @@
 // Your code goes here
+
+document.addEventListener('DOMContentLoaded', function() {
+    let p = document.getElementById('text')
+    p.innerHTML = "<p>This is really cool!</p>"
+})
